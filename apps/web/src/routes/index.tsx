d504fb2a0f8ca78c663919749190b731bca04dd6
Pedro Costa <scosta.pedro.manoel@gmail.com>
2025-05-10 18:29:42 -0300

    chore: setup shadcn-ui

--- apps/web/src/routes/index.tsx
+++ apps/web/src/routes/index.tsx
@@ -1,13 +1,15 @@
-import { createFileRoute } from '@tanstack/react-router'
+import { createFileRoute } from "@tanstack/react-router";
+import { Button } from "@workspace/ui/components/button";
 
-export const Route = createFileRoute('/')({
+export const Route = createFileRoute("/")({
   component: Home,
-})
+});
 
 function Home() {
   return (
     <div className="p-2">
       <h3>Welcome Home!!!</h3>
+      <Button>Click me</Button>
     </div>
-  )
+  );
 }
