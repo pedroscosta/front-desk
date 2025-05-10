72d06c0ead025c110198f920c1c7ad090d23f8f5
Pedro Costa <scosta.pedro.manoel@gmail.com>
2025-05-10 18:32:30 -0300

    chore: remove old package

--- apps/.web/src/lib/live-state.ts
+++
@@ -1,8 +0,0 @@
-import { createClient } from "@repo/live-state/client";
-import type { Router } from "api/router";
-import { schema } from "api/schema";
-
-export const { client, store } = createClient<Router>({
-  url: "ws://localhost:3333/ws",
-  schema,
-});
