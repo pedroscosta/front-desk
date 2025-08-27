1cffa342804060962ba12c0e94eb05c28c50cbaf
Pedro Costa <scosta.pedro.manoel@gmail.com>
2025-08-27 19:01:32 -0300

    feat(ui): add relative time util

--- packages/ui/src/lib/utils.ts
+++ packages/ui/src/lib/utils.ts
@@ -1,6 +1,20 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { type ClassValue, clsx } from "clsx";
+import { differenceInSeconds, formatDistanceToNowStrict } from "date-fns";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
+}
+
+export function formatRelativeTime(
+  date: Date,
+  options?: { minimumDifference?: number }
+) {
+  const secondsAgo = differenceInSeconds(new Date(), date);
+
+  if (secondsAgo < (options?.minimumDifference ?? 30)) {
+    return "now";
+  }
+
+  return formatDistanceToNowStrict(date, { addSuffix: true });
 }
