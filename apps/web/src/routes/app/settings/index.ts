d4033c5b0c1d671c9c0b8668e31f872adbf0d175
Pedro Costa <scosta.pedro.manoel@gmail.com>
2025-09-25 22:57:42 -0300

    feat(web): create settings route and layout

---
+++ apps/web/src/routes/app/settings/index.ts
@@ -0,0 +1,9 @@
+import { createFileRoute, redirect } from "@tanstack/react-router";
+
+export const Route = createFileRoute("/app/settings/")({
+  loader: () => {
+    throw redirect({
+      to: "/app/settings/organization",
+    });
+  },
+});
---
+++ apps/web/src/routes/app/settings/route.tsx
@@ -0,0 +1,18 @@
+import { createFileRoute, Outlet } from "@tanstack/react-router";
+import { Card } from "@workspace/ui/components/card";
+import { SettingsSidebar } from "~/components/sidebar/settings-sidebar";
+
+export const Route = createFileRoute("/app/settings")({
+  component: RouteComponent,
+});
+
+function RouteComponent() {
+  return (
+    <div className="w-screen h-screen flex overflow-hidden">
+      <SettingsSidebar />
+      <Card className="flex-1 bg-muted/30 relative m-2 ml-0 h-auto p-4">
+        <Outlet />
+      </Card>
+    </div>
+  );
+}
